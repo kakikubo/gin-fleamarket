79a7643858bd9e86c715823eaee70018edf32150
kakikubo <kakikubo@gmail.com>
2025-01-26 06:12:47 +0900

    環境変数指定の誤りを修正

--- infra/db.go
+++ infra/db.go
@@ -13,7 +13,7 @@ func SetupDB() *gorm.DB {
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
