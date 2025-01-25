b5602b34f956d9fa0ad6f36210d07403312aed76
kakikubo <kakikubo@gmail.com>
2025-01-26 07:14:27 +0900

    データベースへの登録処理実装

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"gin-fleamarket/controllers"
 	"gin-fleamarket/infra"
-	"gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 	"gin-fleamarket/services"
 	"log"
@@ -14,14 +13,16 @@ import (
 
 func main() {
 	infra.Initialize()
+	db := infra.SetupDB()
 	log.Println(os.Getenv("ENV"))
-	items := []models.Item{
-		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
-		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	}
+	// items := []models.Item{
+	// 	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	// }
 
-	itemRepository := repositories.NewItemMemoryRepository(items)
+	// itemRepository := repositories.NewItemMemoryRepository(items)
+	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
