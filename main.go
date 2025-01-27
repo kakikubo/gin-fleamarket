c238f09bdc6320cb0227f42584315590dacb10a5
kakikubo <kakikubo@gmail.com>
2025-01-28 06:48:30 +0900

    CORSの設定

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,6 +33,7 @@ func main() {
 	authController := controllers.NewAuthController(authService)
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
